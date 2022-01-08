c4da1e569b20f19085c7769593c4a257c34d1885
felipe <ingenierofelipeurrego@gmail.com>
2022-01-08 15:16:26 -0500

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -11,11 +11,9 @@ def predict(input):
     mimg = moleimages.MoleImages()
     X = mimg.load_image(input)
     y_pred = model.predict(X)
-    return {"benign": float(y_pred[0][0]), "cancerous": float(1-y_pred[0][0])}
+    return {"Benigno": float(y_pred[0][0]), "Cancerígeno": float(1-y_pred[0][0])}
 
 examples=[["benign.png"], ["cancerous.png"]]
 
-io = gradio.Interface(fn=predict, inputs='image', outputs='label', capture_session=True, examples=examples, 
-thumbnail="https://raw.githubusercontent.com/gradio-app/hub-skin-cancer/master/thumbnail.png", analytics_enabled=False,
-	title="Identifying Skin Cancer", description="Predicts whether an image of skin is cancerous or not. This model is EXPERIMENTAL and should only be used for research purposes. Please see a doctor for any diagnostic reasons.")
-io.launch()
+io = gradio.Interface(fn=predict, inputs='image', outputs='label', capture_session=True, examples=examples,thumbnail="https://raw.githubusercontent.com/gradio-app/hub-skin-cancer/master/thumbnail.png", analytics_enabled=False,title="IDENTIFICADOR DE CANCER DE PIEL", description="Predice si una imagen de la piel es cancerosa o no. Este modelo es EXPERIMENTAL y sólo debe utilizarse con fines de investigación. Por favor, consulte a un médico por cualquier motivo de diagnóstico.")
+io.launch(share=True)
